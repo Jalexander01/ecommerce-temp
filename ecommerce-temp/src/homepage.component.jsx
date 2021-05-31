19e35d67573206d36d705e2a3246823b8195578b
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-31 16:52:32 -0500

    Create homepage.component.jsx

---
+++ ecommerce-temp/src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className = 'homepage'>
+         <div className = 'directory-menu'>
+        <di className='menu-item'>
+
+        </di>
+         </div>
+    </div>
+)
