b494544de2c9f3343aa6f298af562509a6f57ac3
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-31 17:50:33 -0500

    Setting up projects

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import './homepage.styles.sass';
+import React from "react";
+import "./homepage.styles.scss";
+
 const HomePage = () => (
-    <div className = 'homepage'>
-         <div className = 'directory-menu'>
-        <di className='menu-item'>
-        <div className ='content'>
-         <h1> HATS</h1>
-         <span>CLEARANCE SALE!</span>
+    <div className="homepage">
+        <div className="directory-menu">
+            <di className="menu-item">
+                <div className="content">
+                    <h1> HATS</h1>
+                    <span>CLEARANCE SALE!</span>
+                </div>
+            </di>
+            <di className="menu-item">
+                <div className="content">
+                    <h1> JACKETS</h1>
+                    <span>CLEARANCE SALE!</span>
+                </div>
+            </di>
+            <di className="menu-item">
+                <div className="content">
+                    <h1> WOMEN</h1>
+                    <span>CLEARANCE SALE!</span>
+                </div>
+            </di>
+            <di className="menu-item">
+                <div className="content">
+                    <h1> MENS</h1>
+                    <span>CLEARANCE SALE!</span>
+                </div>
+            </di>
         </div>
-        </di> 
-        <di className='menu-item'>
-        <div className ='content'>
-         <h1> JACKETS</h1>
-         <span>CLEARANCE SALE!</span>
-        </div>
-        </di>
-        <di className='menu-item'>
-        <div className ='content'>
-         <h1> WOMEN</h1>
-         <span>CLEARANCE SALE!</span>
-        </div>
-        </di>
-        <di className='menu-item'>
-        <div className ='content'>
-         <h1> MENS</h1>
-         <span>CLEARANCE SALE!</span>
-        </div>
-        </di>
-         </div>
     </div>
 );
 
