97155769e7adee8291831126a7a5d65122f61b17
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-05-31 17:00:24 -0500

    setting up project

--- ecommerce-temp/src/homepage.component.jsx
+++ ecommerce-temp/src/homepage.component.jsx
@@ -4,8 +4,31 @@ const HomePage = () => (
     <div className = 'homepage'>
          <div className = 'directory-menu'>
         <di className='menu-item'>
-
+        <div className ='content'>
+         <h1> HATS</h1>
+         <span>CLEARANCE SALE!</span>
+        </div>
+        </di>
+        <di className='menu-item'>
+        <div className ='content'>
+         <h1> JACKETS</h1>
+         <span>CLEARANCE SALE!</span>
+        </div>
+        </di>
+        <di className='menu-item'>
+        <div className ='content'>
+         <h1> WOMEN</h1>
+         <span>CLEARANCE SALE!</span>
+        </div>
+        </di>
+        <di className='menu-item'>
+        <div className ='content'>
+         <h1> MENS</h1>
+         <span>CLEARANCE SALE!</span>
+        </div>
         </di>
          </div>
     </div>
-)
+);
+
+export default HomePage;
