c0d488a49d703c60a32458b354d0b7376cf9ae4a
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-06-01 07:32:26 -0500

    updating

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
- 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -10,4 +10,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
- 
